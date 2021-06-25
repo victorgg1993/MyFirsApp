8ca84b3b6dd88de0eb89a48a8014254a950a37d1
victorgg1993 <vigomga1993@gmail.com>
2021-06-25 10:33:20 +0200

    primer commit

---
+++ MyFirstApp/Calculator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstApp
+{
+    public class Calculator
+    {
+        public int Add(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+
+        public int Substract(int num1, int num2)
+        {
+            return num1 - num2;
+        }
+
+        public int Multiply(int num1, int num2)
+        {
+            return num1 * num2;
+        }
+
+
+    }
+}
---
+++ MyFirstAppTests/CalculatorTests.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MyFirstApp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstApp.Tests
+{
+    [TestClass()]
+    public class CalculatorTests
+    {
+        [TestMethod()]
+        public void AddTest()
+        {
+            Calculator calculator = new Calculator();
+            Assert.IsTrue(calculator.Add(2, 2) == 4);
+            //Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void SubstractTest()
+        {
+            Calculator calculator = new Calculator();
+            Assert.IsTrue(calculator.Substract(4, 3) == 1);
+            //Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void MultiplyTest()
+        {
+            Calculator calculator = new Calculator();
+            Assert.IsTrue(calculator.Multiply(2, 3) == 6);
+            //Assert.Fail();
+        }
+    }
+}
