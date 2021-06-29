95851569852aabdb50309f0559381e4375ec9b3e
victorgg1993 <vigomga1993@gmail.com>
2021-06-29 12:44:43 +0200

    For loop imlpementation. closes #3

--- MyFirstApp/ForLoop/frmFormLoop.Designer.cs
+++ MyFirstApp/ForLoop/frmFormLoop.Designer.cs
@@ -29,12 +29,74 @@ namespace MyFirstApp.ForLoop
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.txtAge = new System.Windows.Forms.TextBox();
+            this.txtStudentId = new System.Windows.Forms.TextBox();
+            this.txtSurname = new System.Windows.Forms.TextBox();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtAge
+            // 
+            this.txtAge.Location = new System.Drawing.Point(350, 241);
+            this.txtAge.Name = "txtAge";
+            this.txtAge.Size = new System.Drawing.Size(100, 20);
+            this.txtAge.TabIndex = 9;
+            // 
+            // txtStudentId
+            // 
+            this.txtStudentId.Location = new System.Drawing.Point(350, 124);
+            this.txtStudentId.Name = "txtStudentId";
+            this.txtStudentId.Size = new System.Drawing.Size(100, 20);
+            this.txtStudentId.TabIndex = 8;
+            // 
+            // txtSurname
+            // 
+            this.txtSurname.Location = new System.Drawing.Point(350, 201);
+            this.txtSurname.Name = "txtSurname";
+            this.txtSurname.Size = new System.Drawing.Size(100, 20);
+            this.txtSurname.TabIndex = 7;
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(350, 164);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(100, 20);
+            this.txtName.TabIndex = 6;
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(362, 303);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 5;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // frmFormLoop
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtAge);
+            this.Controls.Add(this.txtStudentId);
+            this.Controls.Add(this.txtSurname);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.btnSave);
+            this.Name = "frmFormLoop";
             this.Text = "frmFormLoop";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox txtAge;
+        private System.Windows.Forms.TextBox txtStudentId;
+        private System.Windows.Forms.TextBox txtSurname;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Button btnSave;
     }
 }
--- MyFirstApp/ForLoop/frmFormLoop.cs
+++ MyFirstApp/ForLoop/frmFormLoop.cs
@@ -1,12 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Configuration;
 using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MyFirstApp.ForeachLoop;
 
 namespace MyFirstApp.ForLoop
 {
@@ -15,6 +17,33 @@ namespace MyFirstApp.ForLoop
         public frmFormLoop()
         {
             InitializeComponent();
+        }
+
+        private void frmForLoop_Load(object sender, EventArgs e)
+        {
+            String sAttr = ConfigurationManager.AppSettings.Get("CsvFile");
+            MessageBox.Show(sAttr);
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            var student = new Student();
+            student.StudentId = Convert.ToInt32(txtStudentId.Text);
+            student.Name = txtName.Text;
+            student.Surname = txtSurname.Text;
+            student.Age = Convert.ToInt32(txtAge.Text);
+
+            //Generic collections
+            List<Student> studentList = new List<Student>(); // recommended 
+
+            studentList.Add(student);
+
+            for (int i = 0; i < studentList.Count; i++)
+            {
+                //Unboxing-Polymorphism
+                MessageBox.Show(studentList[i].Name +
+                                 " " + studentList[i].Surname);
+            }
         }
     }
 }
