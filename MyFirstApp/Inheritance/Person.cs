4f903cabfe95145caa68d4da75330fe420e30815
victorgg1993 <vigomga1993@gmail.com>
2021-07-01 13:11:20 +0200

    polymorphism implementation . Closes #6

---
+++ MyFirstApp/Inheritance/Person.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstApp.Inheritance
+{
+    class Person
+    {
+        public Guid Guid { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+
+        public Person()
+        {
+            Guid = Guid.NewGuid();
+        }
+
+        public Person(string name, string surname) : this()
+        {
+            Name = name;
+            Surname = surname;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is Person person &&
+                base.Equals(obj) &&
+                Guid.Equals(person.Guid) &&
+                Name == person.Name &&
+                Surname == person.Surname;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(base.GetHashCode(),
+                Guid, Name, Surname);
+        }
+
+
+
+    }
+}
---
+++ MyFirstApp/Inheritance/Teacher.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstApp.Inheritance
+{
+    class Teacher : Person
+    {
+        public string Subject { get; set; }
+        public float Salary { get; set; }
+
+        public Teacher()
+        {
+
+        }
+
+        public Teacher(string subject, float salary,
+            string name, string surname) : base(name, surname)
+        {
+            Subject = subject;
+            Salary = salary;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is Teacher teacher &&
+                base.Equals(obj) &&
+                Guid.Equals(teacher.Guid) &&
+                Name == teacher.Name &&
+                Surname == teacher.Surname &&
+                Subject == teacher.Subject &&
+                Salary == teacher.Salary;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(base.GetHashCode(),
+                Guid, Name, Surname, Subject, Salary);
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+}
